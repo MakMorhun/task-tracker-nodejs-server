1c41e2b89771bd4e0e3872cfe47b1979030a8c55
MakMorhun <morgun7575@gmail.com>
2022-10-13 19:29:55 +0300

    Change the date format to string

--- models/task.js
+++ models/task.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const taskSchema = mongoose.Schema({
   text: { type: String, required: true},
-  date: { type: Date, required: true},
+  date: { type: String, required: true},
   reminder: { type: Boolean, required: false}
 })
 
